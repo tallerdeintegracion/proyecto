2e1e919eb9ce2f1b4646368e5fe9b5b7fcd890f9
jpsilvaa <jphinrichsen@wificlient-10-201-120-46.uc.cl>
2016-04-21 22:33:59 -0300

    Documentacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/documentacion'
+
   get 'home/index'
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
