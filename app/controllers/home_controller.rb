a4ab4bf2d1b72a6c3c806ad3f7d5a7d6833e62af
crichard1 <crichard1@uc.cl>
2016-04-24 16:32:41 -0300

    unos metodos mas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,18 @@ class HomeController < ApplicationController
   def index
 
 
-  	#Etaba probando  =)
   	#almacen id example (79 producto)= 571262aaa980ba030058a1f3
     #producto id 571262aaa980ba030058a23c
   	#@show = getAlmacenes()
   	#@show = getSKUWithStock('571262aaa980ba030058a1f3')
   	#@show = getCuentaFabrica()
-  	#@show = getStock('571262aaa980ba030058a1f3' , 49 )
-    @show = moverStock( '571262aaa980ba030058a23c', '571262aaa980ba030058a1f1' )
+  	#@show = getStock('571262aaa980ba030058a1f3' , 49 )  
+    #@show = moverStock( '571262aaa980ba030058a23c', '571262aaa980ba030058a1f3' )
+    #@show = moverStockBodega( '571262aaa980ba030058a23c', '571262aaa980ba030058a1f3' )
+    #@show = obtenerCartola(1429832246 , 1461454686, '571262c3a980ba030058ab5d')
+    #@show = obtenerTransaccion('1234')
+    @show = obtenerCuenta('571262c3a980ba030058ab5d')
+
   	
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,24 @@ module ApplicationHelper
 
 	
 
-	def findKeys
-		key = 'GKTSVmI778e8Mjg'
-		return key
-	end
 	def bodegaBaseUrl
 		return 'http://integracion-2016-dev.herokuapp.com/bodega'
 	end
+	def bancoBaseUrl
+		return 'http://mare.ing.puc.cl/banco'
+	end
+
+
+
+	## Metodos bodega###
+	# -getAlmacenes
+	# -getSKUWithStock
+	# -getStock
+	# -moverStock
+	# -moverStockBodega
+	# - producir(no implementado)
+	# - Despachar(no implementado) 
+	# -getCuentaFabrica
 
 	def getAlmacenes
 
@@ -18,11 +29,9 @@ module ApplicationHelper
 		String toEncode = "GET" 
 		url =bodegaBaseUrl+path
         authHeader = encodeHmac(toEncode)    
-        data =  httpRequest(url , authHeader, "GET")
+        data =  httpGetRequest(url , authHeader)
         return  data
 	end
-
-
 
 	def getSKUWithStock(almacenId)
 	
@@ -30,7 +39,7 @@ module ApplicationHelper
 		url =bodegaBaseUrl+path+"?"+"almacenId="+almacenId
 		String toEncode = "GET"+almacenId 
         authHeader = encodeHmac(toEncode)
-        data =  httpRequest(url, authHeader,"GET")
+        data =  httpGetRequest(url , authHeader)
         return  data
 
 	end
@@ -44,40 +53,31 @@ module ApplicationHelper
 		url =bodegaBaseUrl+path+"?"+"almacenId="+almacenId+'&'+"sku="+sku.to_s+"&limit="+limit.to_s
 		String toEncode = "GET"+almacenId+sku.to_s
         authHeader = encodeHmac(toEncode)
-        data =  httpRequest(url, authHeader,"GET")
+        data =  httpGetRequest(url , authHeader)
         return  data
 
 	end
 
-
-
-
 	def moverStock(productoId , almacenId)
 		path ='/moveStock'
-		url =bodegaBaseUrl+path+"?"+"productoId="+productoId+'&'+"almacenId="+almacenId
-		#render :text => url
+		url =bodegaBaseUrl+path
 		String toEncode = "POST"+productoId+almacenId
-		render :text => toEncode
         authHeader = encodeHmac(toEncode)
-       # render :text => authHeader
-        data =  httpRequest(url, authHeader,"POST")
+        params = {'productoId' => productoId, 'almacenId' => almacenId}
+        data =  httpPostRequest(url , authHeader, params)
         return  data
 	end	
 
 	def moverStockBodega(productoId , almacenId)
-
 		path ='/moveStockBodega'
-		url =bodegaBaseUrl+path+"?"+"productoId="+productoId+'&'+"almacenId="+almacenId
-		#render :text => url
+		url =bodegaBaseUrl+path
 		String toEncode = "POST"+productoId+almacenId
-		
         authHeader = encodeHmac(toEncode)
-        #render :text => authHeader
-        data =  httpRequest(url, authHeader,"POST")
+        params = {'productoId' => productoId, 'almacenId' => almacenId}
+        data =  httpPostRequest(url , authHeader, params)
         return  data
 	end	
 
-    #INTEGRACION grupo3:4duJdOkzEf2a6eycwDXgM5MzU9o=
 
 	def getCuentaFabrica()
 
@@ -85,26 +85,80 @@ module ApplicationHelper
 		String toEncode = "GET"
 		url =bodegaBaseUrl+path
         authHeader = encodeHmac(toEncode)
-        data =  httpRequest(url, authHeader,"GET" )
+        data =  httpGetRequest(url , authHeader)
         return  data
 
 	end
 
+	# Banco#
+	# => obtenertransaccion
+	# => obtener cartola
+	# => obtener cuenta
+	# => transferir
+
+	def obtenerTransaccion(id)
+		path ='/trx/'+id
+		url =bancoBaseUrl+path
+		data =  httpGetRequest(url ,nil)
+        return  data
+	end
+	def obtenerCartola(fechaInicio , fechaFin, id)
+
+		path ='/cartola'
+		url =bancoBaseUrl+path
+		params = {"fechaInicio": fechaInicio,"fechaFin": fechaFin,"id": id}
+		data =  httpPostRequest(url , nil, params)
+		return  data
+	end
+	def obtenerCuenta(id)
+		path ='/cuenta/'+id
+		url =bancoBaseUrl+path
+		data =  httpGetRequest(url ,nil)
+        return  data
+	
+	end
 
 
-	def httpRequest( url, authHeader, type )
+
+
+
+	def httpPostRequest( url, authHeader ,params)
 		require 'net/http'
 		require 'uri'
+		if authHeader.nil?
+			headers = {'Content-Type' => 'application/json'}
+		else
 		headers = {'Authorization' => authHeader , 'Content-Type' => 'application/json'}
-        uri = URI.parse(url)    
-        if type=="GET"
-        request = Net::HTTP::Get.new(uri, headers)
-        elsif type=="POST"
-        request = Net::HTTP::Post.new(uri, headers)
-        end	
-        response = Net::HTTP.new(uri.host, 80).start {|http| http.request(request) }
-    	data = response.body
+		end	
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+		response = http.post(uri.path, params.to_json, headers)
 
+    	data = response.body
+	end
+
+
+	def httpGetRequest( url, authHeader)
+		require 'net/http'
+		require 'uri'
+		
+		if authHeader.nil?
+			headers = {'Content-Type' => 'application/json'}
+		else
+			headers = {'Authorization' => authHeader , 'Content-Type' => 'application/json'}
+		end	
+
+        uri = URI.parse(url)    
+        request = Net::HTTP::Get.new(uri, headers)
+        response = Net::HTTP.new(uri.host, uri.port).start {|http| http.request(request) }
+    	data = response.body
+	end
+
+
+
+	def findKeys
+		key = 'GKTSVmI778e8Mjg'
+		return key
 	end
 
 	def encodeHmac(data )
@@ -117,6 +171,12 @@ module ApplicationHelper
 
         return result
 	end
+
+
+
+
+
+
 
 
 end
