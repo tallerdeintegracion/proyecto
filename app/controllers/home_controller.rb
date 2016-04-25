9204e3901113b3b3e309ae1779cf0359063fd365
crichard1 <crichard1@uc.cl>
2016-04-24 23:52:02 -0300

    otros metodos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
 
   	#almacen id example (79 producto)= 571262aaa980ba030058a1f3
     #producto id 571262aaa980ba030058a23c
+    #id orden de compra 571b745e79749603006e3eb2
   	#@show = getAlmacenes()
   	#@show = getSKUWithStock('571262aaa980ba030058a1f3')
   	#@show = getCuentaFabrica()
@@ -14,8 +15,10 @@ class HomeController < ApplicationController
     #@show = moverStockBodega( '571262aaa980ba030058a23c', '571262aaa980ba030058a1f3' )
     #@show = obtenerCartola(1429832246 , 1461454686, '571262c3a980ba030058ab5d')
     #@show = obtenerTransaccion('1234')
-    @show = obtenerCuenta('571262c3a980ba030058ab5d')
-
-  	
+    #@show = obtenerCuenta('571262c3a980ba030058ab5d')
+    #@show = recepcionarOrdenDeCompra('571b745e79749603006e3eb2')
+    #@show = obtenerOrdenDeCompra('571b745e79749603006e3eb2')
+    #@show = rechazarOrdenDeCompra('57128e450606410300c3fbeb' , "no hay tiempo")
+    @show = obtenerFactura('571b745e79749603006e3eb2')  	
   end
 end
