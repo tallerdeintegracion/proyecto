0434eb1f983e96b01fa57fe0c5c9c2c1aaa06baa
crichard1 <crichard1@uc.cl>
2016-04-26 19:29:33 -0300

    avanses de busqueda de pedido

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem 'whenever','~> 0.9.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem "passenger"
 
-
+gem 'net-sftp'
+gem 'net-ssh', '~> 3.0', '>= 3.0.2'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/receive_orders_controller.rb
+++ app/controllers/receive_orders_controller.rb
@@ -1,7 +1,68 @@
 class ReceiveOrdersController < ApplicationController
   
+
 def self.run
-  puts "#{Time.now} - iniciar descarga de pedidos"
-  
+ 	
+  	
+  	require 'nokogiri'
+
+ 	puts "#{Time.now} - iniciar descarga de pedidos"
+
+ 	serverDevCredentials()
+	connect()
+	
+
+     # grab data off the remote host directly to a buffer
+    #files = @sftp.download!('/pedidos')
+
+   		
+   
 end
+
+
+private
+   def self.connect()
+   		require 'net/ssh'
+    	require 'net/sftp'
+    	require 'rubygems'
+    	require 'stringio'
+
+        p 'ESTABLISH SFTP CONNECTION'
+        host = 'mare.ing.puc.cl'
+        username = 'integra3'
+        password = 'm3fzRPd5'
+        port = 22
+
+        sftp = Net::SFTP.start(host, username, :password => password) 
+ 		 # grab data off the remote host directly to a buffer
+ 		
+ 		# create a directory
+  		
+  		#data = sftp.download!("/pedidos" , "/home/cristobal/Archivos" , :recursive => true)
+
+  		sftp.dir.foreach("/pedidos") do |entry|
+  		puts entry.name
+  		io = StringIO.new
+  		data = sftp.download!("/pedidos/"+ entry.name , io)
+		end
+
+  		# grab data off the remote host directly to a buffer
+	 	 #data = sftp.download!("sftp://mare.ing.puc.cl/pedidos")
+
+
+       	
+        p 'CONNECTION ESTABLISHED '
+end 
+
+def self.serverDevCredentials
+	@server = "mare.ing.puc.cl"
+  	@user = "integra3"
+	@password = "m3fzRPd5"	
+end
+
+def getOrdenDeCompra
+	
+end
+
+
 end
