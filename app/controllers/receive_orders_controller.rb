32c0e7f61d32956ad9f42fa66904fa186cb91a2c
crichard1 <crichard1@uc.cl>
2016-04-27 13:27:02 -0300

    descarga de Xmls

--- app/controllers/receive_orders_controller.rb
+++ app/controllers/receive_orders_controller.rb
@@ -3,66 +3,66 @@ class ReceiveOrdersController < ApplicationController
 
 def self.run
  	
-  	
-  	require 'nokogiri'
+  	require 'net/ssh'
+    require 'net/sftp'
+    require 'rubygems'
+    require 'stringio'
+    require 'tempfile'
+    require 'nokogiri'	
+
 
  	puts "#{Time.now} - iniciar descarga de pedidos"
+	sftp =connect()
+	searchOrders(sftp)
 
- 	serverDevCredentials()
-	connect()
-	
-
-     # grab data off the remote host directly to a buffer
-    #files = @sftp.download!('/pedidos')
-
-   		
    
 end
 
 
-private
-   def self.connect()
-   		require 'net/ssh'
-    	require 'net/sftp'
-    	require 'rubygems'
-    	require 'stringio'
+def self.connect()
+   	
 
-        p 'ESTABLISH SFTP CONNECTION'
-        host = 'mare.ing.puc.cl'
-        username = 'integra3'
-        password = 'm3fzRPd5'
-        port = 22
+    #cambiar a produccion
+    p 'ESTABLISH SFTP CONNECTION'
+    host = 'mare.ing.puc.cl'
+    username = 'integra3'
+    password = 'm3fzRPd5'
+    port = 22
 
-        sftp = Net::SFTP.start(host, username, :password => password) 
- 		 # grab data off the remote host directly to a buffer
- 		
- 		# create a directory
-  		
-  		#data = sftp.download!("/pedidos" , "/home/cristobal/Archivos" , :recursive => true)
-
-  		sftp.dir.foreach("/pedidos") do |entry|
-  		puts entry.name
-  		io = StringIO.new
-  		data = sftp.download!("/pedidos/"+ entry.name , io)
-		end
-
-  		# grab data off the remote host directly to a buffer
-	 	 #data = sftp.download!("sftp://mare.ing.puc.cl/pedidos")
-
-
-       	
-        p 'CONNECTION ESTABLISHED '
+    sftp = Net::SFTP.start(host, username, :password => password) 
+ 	
+	
 end 
 
-def self.serverDevCredentials
-	@server = "mare.ing.puc.cl"
-  	@user = "integra3"
-	@password = "m3fzRPd5"	
-end
 
-def getOrdenDeCompra
+
+def self.searchOrders (sftp)
 	
+		# create a temporary directory 
+  		Dir.mktmpdir do |dir|
+  		data = sftp.download!("/pedidos" , dir , :recursive => true)
+
+  			Dir.glob(dir+"/*.xml") do |fname|
+  			# do work on files ending in .xml in the desired directory
+  			#puts fname
+
+  			doc = Nokogiri::XML(File.open(fname))
+  			sku=doc.css("order sku").first.text
+  			id=doc.css("order id").first.text
+  			qty=doc.css("order qty").first.text
+  			processOrder(id , sku , qty)
+			end
+  			
+  		end
+
+	puts "#{Time.now} - Pedidos procesados \n"
+
 end
 
+def self.processOrder(id , sku , cantidad)
+
+puts "Trabajando en la orden id: "+ id+ "   sku: "+sku+"   cantidad: "+cantidad+"\n"
+
+end
 
 end
