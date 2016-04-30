3e9a6379a5a06aefbc0fcc99a918a4ab5fcf8d36
crichard1 <crichard1@uc.cl>
2016-04-30 14:34:01 -0300

    arreglo

--- app/controllers/inventario_controller.rb
+++ app/controllers/inventario_controller.rb
@@ -15,26 +15,34 @@ class InventarioController < ApplicationController
   
   def self.run
   
-  @returnPoint = 500
-  @bodegaPrincipal = "571262aaa980ba030058a1f3"
-  @cuentaGrupo = "571262c3a980ba030058ab5d"
-  @cuentaFabrica = JSON.parse(getCuentaFabrica)["cuentaId"]
-  puts "Inicia revicion de inventario "
+  definirVariables 
+  puts "\n \n #{Time.now}  Inicia revicion de inventario "
   
   cleanProduccionesDespachadas()
+  recibirMateriasPrimas(@bodegaRecepcion , @bodegaPrincipal)
   checkMateriasPrimas(@bodegaPrincipal)
   end
 	
+  def self.definirVariables 
+  		
+  	@returnPoint = 400
+  	@bodegaPrincipal = "571262aaa980ba030058a1f3"
+  	@bodegaRecepcion = "571262aaa980ba030058a1f1"
+  	@cuentaGrupo = "571262c3a980ba030058ab5d"
+  	@cuentaFabrica = JSON.parse(getCuentaFabrica)["cuentaId"]
+
+  end	
   def self.checkMateriasPrimas(bodega)
 
   ##TODO sacar info de bases de datos y replicas 
+  puts "3) #{Time.now}  Revisando si son necesarias materias primas "
   sku = "8"	
   coste =1313
   tamañoLote = 100
 
 
   stock = checkStock(sku, bodega)
-  puts "### Stock del " + sku.to_s + " es de " + stock.to_s 
+  puts "### Stock del sku " + sku.to_s + " es de " + stock.to_s 
 	
   produccion = enProduccion(sku)
   puts "### Existen en produccion " + produccion.to_s
@@ -53,7 +61,6 @@ class InventarioController < ApplicationController
   detallesProd = producir(sku, trx, cantidad)
   p detallesProd
 
- 	 ### Agregarhistorial de producciones
  actualizarRegistoProduccion(detallesProd , sku , cantidad)
  	 
 
@@ -85,20 +92,32 @@ class InventarioController < ApplicationController
 
 
   end	
+
   def self.checkStock(sku , bodega)
   	require 'json'
   	result = JSON.parse(getSKUWithStock(bodega))
+  
+  	if result.nil?
+  		return 0
+  	end	
+
   	stock =0
 	for counter in 0..(result.length-1)
 
-		actualSku =result[counter]['_id'].to_i
-		actualTotal =result[counter]['total'].to_i
+		actualSku =result[counter]['_id'].to_s
 
 		if(sku == actualSku)
+			actualTotal =result[counter]['total'].to_i
 			stock = actualTotal 
 		end
 	end
-	return actualTotal
+
+	if stock.nil?
+		# Error 
+		return 9000000 
+	end
+
+	return stock
   		
   end
 
@@ -129,33 +148,58 @@ class InventarioController < ApplicationController
 
 
   def self.cleanProduccionesDespachadas()
-  	
+  	puts "1) #{Time.now}  Limpiando producciones despachadas del registro "
   	time = Time.now
-  	produccion = Production.where( 'disponible <= ?', time ).destroy_all
-	
+  	produccion = Production.where( 'disponible <= ?', time )
+	puts "### Fueron despachadas " + produccion.count.to_s
+	produccion.destroy_all
   end	
 
 
 
-  def self.producirMateriaPrima(sku , lotes ,tamañoLote , costoUnitario)
+  def self.recibirMateriasPrimas( almacenRecepcion , bodegaMateriasPrimas)
+  		puts "2) #{Time.now} Reciviendo materias primas "
+  		sku= 8 
+  		stock = checkStock(sku ,almacenRecepcion) 
+  		
+  		puts "### Stock de " + stock.to_s + " disponibles en almacen de recepcion"	
 
-  	cuentaGrupo = "571262c3a980ba030058ab5d"
-  	cuentaFabrica =  JSON.parse(getCuentaFabrica)["cuentaId"]
-  	monto= lotes*(tamañoLote*costoUnitario)
+  		if stock == 0
+  			return
+  		end
 
+
+
+  		while stock > 0
+  			stock = checkStock(sku ,almacenRecepcion)
+			resp = JSON.parse(getStock(almacenRecepcion , sku , 200 ))
+			cantidadMover = resp.length
+			
+			if resp.nil? | cantidadMover.nil? 
+  			return
+  			end
+			moveProducts(resp ,cantidadMover,  bodegaMateriasPrimas )
+			
+  		end	
   
 
-  	#trx = JSON.parse(transferir(monto , cuentaGrupo, cuentaFabrica))['_id']
-  	#if trx.nil?
-  	#	p Error en la transaccion 
-  	#	return	
-  	#end
-  	
-  	#prod =producirStock(sku , trx , lotes*tamañoLote)
-  	#p prod
- 	Production.updateProduction(lotes*tamañoLote , sku)
+  end 	
 
-  end
+  def self.moveProducts(products , cantidad , destino)
+  	puts "### Moviendo  " + cantidad.to_s + " a bodega principal"
+  	
+  	if cantidad.nil? | products.nil? | cantidad==0
+  		return
+  	end	
+
+  	for counter in 0..cantidad-1
+  		id = products[counter]["_id"]
+  		moverStock(id , destino)
+  	end
+
+  end	
+
+ 
 
   
 #572283e304c78e0300ce3ee2"
