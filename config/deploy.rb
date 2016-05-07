ffdf625a1e49cdcc899eafaf996894398083cd7c
jpsilvaa <jphinrichsen@wificlient-10-201-120-46.uc.cl>
2016-05-07 12:58:28 -0300

    Cambios

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :ssh_options, { :forward_agent => true }
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, "master"
+set :branch, "basesDatos"
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
 set :deploy_to, '/home/administrator/Proyecto'
