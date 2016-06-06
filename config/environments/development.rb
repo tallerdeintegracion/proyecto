f8cc499e31ec99799b75dd76e677fb1a0ab2ab70
jpsilvaa <jphinrichsen@wificlient-10-201-120-46.uc.cl>
2016-06-05 20:44:00 -0400

    Fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,6 @@ Rails.application.configure do
   
   config.serve_static_files = true
 
+  config.action_mailer.default_url_options = { :host => "integra3.ing.puc.cl" }
+  config.action_mailer.delivery_method = :sendmail
 end
