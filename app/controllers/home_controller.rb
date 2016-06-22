69cc6f1a3dc6adc71e5b27733d6d1361618d7b6a
bagimene <bagimene@uc.cl>
2016-06-22 17:31:57 -0400

    graficoPieNada

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,59 +31,28 @@ class HomeController < ApplicationController
       render :text => "holia"
   end
 
-  #def dashboard
-    
-=begin 
-      @data = {
-      labels: ["January","February","March","April","May","June","July"],
-      datasets: [
-              {
-                  fillColor: "rgba(220,220,220,0.5)",
-                  strokeColor: "rgba(220,220,220,1)",
-                  pointColor: "rgba(220,220,220,1)",
-                  pointStrokeColor: "#fff",
-                  data: [65,59,90,81,56,55,40]
-              },
-              {
-                  fillColor: "rgba(151,187,205,0.5)",
-                  strokeColor: "rgba(151,187,205,1)",
-                  pointColor: "rgba(151,187,205,1)",
-                  pointStrokeColor: "#fff",
-                  data: [28,48,40,19,96,27,100]
-              }
-          ]
-      }
-      @options = {}
-
-      #<%= line_chart @data, @options %>
-=end
-
-      #render :text => "dashboard"
-  #end
-
   def datosDashboard
 
-    @data = {
-      labels: ["January","February","March","April","May","June","July"],
-      datasets: [
-              {
-                  fillColor: "rgba(220,220,220,0.5)",
-                  strokeColor: "rgba(220,220,220,1)",
-                  pointColor: "rgba(220,220,220,1)",
-                  pointStrokeColor: "#fff",
-                  data: [65,59,90,81,56,55,40]
-              },
-              {
-                  fillColor: "rgba(151,187,205,0.5)",
-                  strokeColor: "rgba(151,187,205,1)",
-                  pointColor: "rgba(151,187,205,1)",
-                  pointStrokeColor: "#fff",
-                  data: [28,48,40,19,96,27,100]
-              }
-          ]
+      @pieData = [
+      {
+          value: 20,
+          color:"#878BB6"
+      },
+      {
+          value: 40,
+          color: "#4ACAB4"
+      },
+      {
+          value: 10,
+          color: "#FF8153"
+      },
+      {
+          value: 30,
+          color: "#FFEA88"
       }
-      @options = {}
-      
+      ];
+      #@options = {}
+      render :text => "hola"
   end
 
 end
